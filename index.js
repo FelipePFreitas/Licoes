2ee1b83032a694ce2e3dd901471955fad320a3e7
Felipe Freitas <felipefreitas210891@gmail.com>
2024-01-23 19:24:20 -0300

    licao complementar (aula02)-Criar um algoritmo JS simples usando outros exemplos

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var nome = prompt("Insira seu nome")
 var idade = prompt("Insira sua idade")
+var saudacao = "Ola Mundo"
+var QualquerNumero = 50
+
 alert(nome+idade)
+console.log(nome+saudacao)
+console.log(idade*QualquerNumero)
