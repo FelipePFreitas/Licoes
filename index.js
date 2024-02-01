f1b074cef68ecd1334de8aae8b9339da409dc5a3
Felipe Freitas <felipefreitas210891@gmail.com>
2024-02-01 19:48:58 -0300

    correção lição

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var nome = prompt("Insira seu nome")
-var idade = prompt("Insira sua idade")
-var saudacao = "Ola Mundo"
-var QualquerNumero = 50
+// Correção de exercício - Atividade Extra
 
-alert(nome+idade)
-console.log(nome+saudacao)
-console.log(idade*QualquerNumero)
+// Solicita um número através do prompt
+
+var numero = prompt("Digite um número:");
+
+// Converte o input para um número
+var numeroInt = parseInt(numero);
+
+
+// Verifica se está entre 10 e 50
+    if(numeroInt >= 10 && numero <= 50) {
+        alert("O número está entre 10 e 50!");
+    } else {
+        console.log("O número não está entre 10 e 50");
+    }
