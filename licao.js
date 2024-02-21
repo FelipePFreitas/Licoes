d5418ded6d36c3b745b5cf550de2251b4e3bf5da
Felipe Freitas <felipefreitas210891@gmail.com>
2024-02-20 21:47:32 -0300

    Aula 8 - Objetos

--- licao.js
+++ licao.js
@@ -1,10 +1,33 @@
-let numero = Number(prompt("Insira um numero"))
-if(numero <= 10){
-    console.log("Numero entre 1 e 10")
+// function maca (cor, forma, temSemente, estacao)
+// { 
+//     this.cor = cor;
+//     this.forma = forma;
+//     this.temSemente = temSemente;
+//     this.estacao = estacao;
+//  }
+// const maca1 = new maca ("Vermelha", "Arrendondado", "Sim", "Outono");
+// // console.log(maca1)
+// // console.log(maca1.cor.length);
+// // console.log(maca1.forma.toUpperCase())
+// // console.log(maca1.estacao.toLowerCase())
+// // maca1.completo()
+
+// for(const propriedade in maca1){
+//     console.log(maca1[propriedade])
+// }
+
+class carro {
+    constructor(marca, cor, modelo, ano){
+        this.marca = marca;
+        this.cor = cor;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    Apresentar(){
+        console.log(`A marca do carro é: ${this.marca+" "+this.cor+" "+this.modelo +" "+ this.ano}`)
+    }
 }
-else if (numero > 10 &&  numero <= 20){
-    console.log("Numero entre 10 e 20")
-}
-else if (numero >20 && numero <= 30){
-    console.log("Numero entre 20 e 30")
-}
+const Meucarro = new carro ("Volks", "Preto", "Polo", "2007")
+const Meucarro2 = new carro ("Volks", "Cinza", "Voyage", "2017")
+Meucarro.Apresentar()
+Meucarro2.Apresentar()
