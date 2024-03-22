c0bd884ebd35f037f2e7e077c8c839bde9ff8dea
Felipe Freitas <felipefreitas210891@gmail.com>
2024-03-21 21:24:48 -0300

    Aula 02 - exemplo pratico

---
+++ licao.js
@@ -0,0 +1,14 @@
+// let nome = "Felipe"
+// let sobrenome = "Freitas"
+// let ano = 2024
+// let idade = 33
+
+// console.log(nome + " " + sobrenome)
+// console.log(ano - idade)
+
+let primeiroNumero = parseInt(prompt("Primeiro Numero"))
+let segundoNumero = parseInt(prompt("Segundo Numero"))
+// let soma = primeiroNumero + segundoNumero
+let multiplicação = primeiroNumero * segundoNumero
+// alert(soma)
+alert(multiplicação)
