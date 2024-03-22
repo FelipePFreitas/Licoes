d4c1bded38877a943be9781f90b7b00cfa13807e
Felipe Freitas <felipefreitas210891@gmail.com>
2024-03-21 21:38:10 -0300

    Entregavel - Aula 2

--- licao.js
+++ licao.js
@@ -6,9 +6,16 @@
 // console.log(nome + " " + sobrenome)
 // console.log(ano - idade)
 
-let primeiroNumero = parseInt(prompt("Primeiro Numero"))
-let segundoNumero = parseInt(prompt("Segundo Numero"))
-// let soma = primeiroNumero + segundoNumero
-let multiplicação = primeiroNumero * segundoNumero
-// alert(soma)
-alert(multiplicação)
+// let primeiroNumero = parseInt(prompt("Primeiro Numero"))
+// let segundoNumero = parseInt(prompt("Segundo Numero"))
+// // let soma = primeiroNumero + segundoNumero
+// let multiplicação = primeiroNumero * segundoNumero
+// // alert(soma)
+// alert(multiplicação)
+
+let nome = prompt("Insira seu nome")
+let sobrenome = prompt("Insira seu sobrenome")
+let anoNascimento = parseInt(prompt("Insira o Ano de nascimento"))
+let nomeCompleto = nome +" "+ sobrenome
+let idade = 2024 - anoNascimento
+alert("Nome completo e sua idade é:" +" "+ nomeCompleto +" "+ idade+"Anos")
