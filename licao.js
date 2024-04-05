cc40932fa0863dda252b1e73482274e0c950fb35
Felipe Freitas <felipefreitas210891@gmail.com>
2024-04-04 21:13:38 -0300

    Funçõe

--- licao.js
+++ licao.js
@@ -56,3 +56,51 @@
 // }else {
 //     alert("Numero fora dos padrões")
 // }
+
+// function recebernumero(){
+//     let numero = prompt("digite um numero");
+//     if(numero % 2 == 0){
+//         alert("Numero é Par")
+
+//     }
+//     else{
+//         alert("Numero impar")
+//     }
+// }
+
+// recebernumero();
+
+// function subtrair(num1 , num2){
+//     let resultado;
+//     if(num1 > num2){
+//         resultado = num1 - num2
+//         console.log("O primeiro numero digitado é maior que o segundo e a subtração entre eles é " + resultado)
+//     }
+//     else{
+//         resultado = num2 - num1
+//         console.log("O segundo numero digitado é maior que o primeiro e a subtração entre eles é " + resultado)
+//     }
+// }
+// let numero1 = prompt("Digite o primeiro número:")
+// let numero2 = prompt("Digite o segundo número:")
+
+// subtrair(numero1, numero2)
+
+// var resultado = (a, b, op) =>{
+//     if (op == "+") {
+//         return a + b
+//     }
+//     else if (op == "-"){
+//         return a - b
+//     }
+//     else if (op == "*"){
+//        return a * b
+//     }
+//     else if (op == "/"){
+//        return a / b
+//     }
+//     else{
+//         return 0
+//     }
+// }
+// console.log( resultado(10, 5, "/") )
